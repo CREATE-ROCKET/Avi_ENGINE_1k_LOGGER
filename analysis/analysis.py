3563cfeedd43b59c870014404f309c13ba2c192c
YutoGCN <61838168+YutoGCN@users.noreply.github.com>
2023-05-18 19:16:14 +0900

    低温用・高温用サーミスタの数を実運用に合わせて変更

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -16,7 +16,7 @@ def main(source,target):
              writer.writerow(['書き込み待ちデータ数','データ取得開始時','データ取得終了時','推力[N]','圧力1[Pa]','圧力2[Pa]','圧力3[Pa]','圧力4[Pa]','高域温度1','高域温度2','低域温度1','低域温度2','低域温度3'])
              for row in tqdm(reader):
                 # 1行ずつ読み込み
-                writer.writerow([row[0],row[1],row[2],str(bin2ThrustTest2(int(row[3]))),str(bin2dataPressure(int(row[5]))),str(bin2dataPressure(int(row[6]))),str(bin2dataPressure(int(row[7]))),str(bin2dataPressure(int(row[8]))),str(bin2dataTempHigh(int(row[11]))),str(bin2dataTempHigh(int(row[12]))),str(bin2dataTempLow(int(row[13]))),str(bin2dataTempLow(int(row[14]))),str(bin2dataTempLow(int(row[15])))])
+                writer.writerow([row[0],row[1],row[2],str(bin2ThrustTest2(int(row[3]))),str(bin2dataPressure(int(row[5]))),str(bin2dataPressure(int(row[6]))),str(bin2dataPressure(int(row[7]))),str(bin2dataPressure(int(row[8]))),str(bin2dataTempLow(int(row[11]))),str(bin2dataTempLow(int(row[12]))),str(bin2dataTempLow(int(row[13]))),str(bin2dataTempHigh(int(row[14]))),str(bin2dataTempHigh(int(row[15])))])
             
     
 
@@ -66,6 +66,7 @@ def bin2dataTemp(binary,R):
 
     T = 1/(1/B * math.log(R_T/R0)+ 1/T0)
     T -= 273.15
+    return T
 
 '''
 def bin2dataTemp(binary,R):
